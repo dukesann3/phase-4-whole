64ed310642e4b096dd8db0174b018150342ebe42
Duke <dukemorise33@gmail.com>
2024-02-13 22:38:03 -0800

    added routing to js

--- server/app.py
+++ server/app.py
@@ -20,6 +20,25 @@ class Employees(Resource):
     def get(self):
         all_employees = [employee.to_dict(rules=('-assignments',)) for employee in Employee.query.all()]
         return make_response(all_employees, 200)
+
+    def post(self):
+        response = request.get_json()
+
+        try:
+            new_employee = Employee(
+                first_name=response["first_name"],
+                last_name=response["last_name"],
+                department=response["department"],
+                role=response["role"]
+            )
+            db.session.add(new_employee)
+            db.session.commit()
+
+            return make_response(new_employee.to_dict(), 201)
+        except:
+            return make_response({"message": "Error, could not create new employee"}, 403)
+
+
     
 class Projects(Resource):
 
@@ -32,11 +51,21 @@ class Assignments(Resource):
     def get(self):
         all_assignments = [assignment.to_dict(rules=('-employee','-project')) for assignment in Assignment.query.all()]
         return make_response(all_assignments, 200)
+    
+class AssignmentID(Resource):
+
+    def get(self, id):
+        assignment = Assignment.query.filter_by(id=id).first().to_dict()
+        if assignment:
+            return make_response(assignment, 200)
+        else:
+            return make_response({"message": f"Error. Could not find assignment with ID: {id}"})
 
 
 api.add_resource(Employees, '/employees')
 api.add_resource(Projects, '/projects')
 api.add_resource(Assignments, '/assignments')
+api.add_resource(AssignmentID, '/assignments/<int:id>')
 
 
 if __name__ == "__main__":
--- server/app_test.py
+++ server/app_test.py
@@ -1,6 +1,7 @@
-from models import Employee
+from models import Employee, Project, Assignment
 from app import app, db
 from faker import Faker
+from datetime import date, timedelta
 
 fake = Faker()
 
@@ -8,7 +9,7 @@ class TestApp:
     '''Flask application in app.py'''
 
     def test_gets_employees(self):
-        '''retrieves campers with GET requests to /campers.'''
+        '''retrieves employees with GET requests to /employees.'''
         with app.app_context():
     
             employee1 = Employee(first_name="Travis", last_name="Browne", department="Engineering", role="Project Engineer")
@@ -39,6 +40,53 @@ class TestApp:
             assert [emp["role"] for emp in response] == [
                 employee.role for employee in employees
             ]
+
+    def test_gets_projects(self):
+        '''retrieves projects with GET request to /projects.'''
+        with app.app_context():
+
+            project_1 = Project(sales_order=453567, name="Hanwa Project", start_date=date(2020,12,20), expected_end_date=date(2021,3,4),
+                              customer_name="Hanwa Ocean", sale_price=500000.00, comment="This is a customer from China")
+            
+            project_2 = Project(sales_order=453498, name="KSOE Project", start_date=date(2019,2,2), expected_end_date=date(2024,2,13),
+                              customer_name="KSOE", sale_price=1000000.00, comment="This is a customer from South Korea")
+            
+            db.session.add_all([project_1, project_2])
+            db.session.commit()
+
+            response = app.test_client().get('/projects')
+            assert response.status_code == 200
+            assert response.content_type == 'application/json'
+
+            response = response.json
+            projects = Project.query.all()
+
+            assert [prj["sales_order"] for prj in response] == [
+                project.sales_order for project in projects
+            ]
+            assert [prj["name"] for prj in response] == [
+                project.name for project in projects
+            ]
+            assert [prj["start_date"] for prj in response] == [
+                str(project.start_date) for project in projects
+            ]
+            assert [prj["expected_end_date"] for prj in response] == [
+                str(project.expected_end_date) for project in projects
+            ]
+            assert [prj["customer_name"] for prj in response] == [
+                project.customer_name for project in projects
+            ]
+            assert [prj["sale_price"] for prj in response] == [
+                project.sale_price for project in projects
+            ]
+            assert [prj["comment"] for prj in response] == [
+                project.comment for project in projects
+            ]
+
+    def test_gets_assignments(self):
+        '''retrieves assignments with GET request /assignments'''
+        with app.app_context():
+            pass
 
 
 
